f750d00f6d701140e4d179108e206e073433968c
nitro <nitromacchitato@users.noreply.github.com>
2021-03-30 15:33:55 -0400

    basic template setup

---
+++ flashcard-app/pages/index.js
@@ -0,0 +1,24 @@
+import 'bulma/css/bulma.css'
+
+export default function Home(){
+
+    return(
+        <>
+            <section className="hero is-fullheight">
+                <div className="hero-head center">
+                    <p>Title</p>
+                </div>
+
+
+                <div className="hero-body box center">
+                    <p>Body</p>             
+                </div>
+
+
+                <div className="hero-foot center">
+                    <p>Footer</p>
+                </div>
+            </section>
+        </>
+    )
+}
