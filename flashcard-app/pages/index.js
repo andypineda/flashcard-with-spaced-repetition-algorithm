fc85764f92918a8fcad56789ee204c025b47ddcf
nitro <nitromacchitato@users.noreply.github.com>
2021-03-30 15:51:04 -0400

    created index page with basic information. using Bulma CSS

--- flashcard-app/pages/index.js
+++ flashcard-app/pages/index.js
@@ -1,22 +1,40 @@
 import 'bulma/css/bulma.css'
+import styles from '../styles/Home.module.css'
+
 
 export default function Home(){
 
     return(
         <>
-            <section className="hero is-fullheight">
+            <section className="hero is-fullheight is-info">
                 <div className="hero-head center">
-                    <p>Title</p>
+                    <div className={`block ${styles.title}`}>
+                        <p className="title">Flashcards</p>
+                    </div>
                 </div>
 
 
-                <div className="hero-body box center">
-                    <p>Body</p>             
+                <div className={`hero-body box center ${styles.intro}`}>
+                    <div className="block">
+                        <p className="title has-text-black">Welcome</p>
+                    </div>
+
+                    <div className="block has-text-centered">
+                        <p>
+                            Spaced repetition is a technique for efficient memorization which uses repeated review of content following a 
+                            schedule determined by a spaced repetition algorithm to improve long-term retention.
+                        </p>
+                    </div>
+
+                    <div className="block">
+                        <button class="button is-success">Create Flashcards</button>
+                    </div>
+                             
                 </div>
 
 
                 <div className="hero-foot center">
-                    <p>Footer</p>
+                    <p>Github</p>
                 </div>
             </section>
         </>
