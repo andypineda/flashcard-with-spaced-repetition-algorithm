9105d707ebdcb813b16d36cd787c383d85b41b69
nitro <nitromacchitato@users.noreply.github.com>
2021-04-12 16:12:44 -0400

    can now add questions and be tested on them using Spaced repetition and recieve a final score

--- flashcard-app/components/card.js
+++ flashcard-app/components/card.js
@@ -1,7 +1,7 @@
+import create from '../styles/Create.module.css'
 
 
-
-export default function Card({questionText}){
+export default function FlashCardComponent({questionText}){
 
 
     return(
@@ -24,45 +24,33 @@ export default function Card({questionText}){
 
                 <div id="answer_box_1" className="control withRadioButton">
                     <div className="control RadioButtonBox">
-                        <input id="question_1" className="control RadioButton" type="radio" />
+                        <input className="control RadioButton" type="radio" />
                     </div>
                     <div className="control answers">
-                        <input id="question_1_answer" class="input" type="text" placeholder="Enter an Answer"></input>
+                        <input class="input" type="text" placeholder="Enter an Answer"></input>
                     </div>
                 </div>
 
 
                 <div id="answer_box_2" className="control withRadioButton">
                     <div className="control RadioButtonBox">
-                        <input id="question_2" className="control RadioButton" type="radio"/>
+                        <input className="control RadioButton" type="radio"/>
                     </div>
                     <div className="control answers">
-                        <input id="question_2_answer" class="input" type="text" placeholder="Enter an Answer"></input>
+                        <input  class="input" type="text" placeholder="Enter an Answer"></input>
                     </div>
                 </div>
 
             </div>
             
 
-            {/* ADD ANOTHER QUESTION BUTTON */}
-            <div className="block">
-                <button id="addQuestionButton" class="button is-info is-light" onClick={AddAnotherQuestion}>Add Another Question</button>
-            </div>
-
-
             {/* ADD AND REMOVE FLASHCARD BUTTON */}
             <div>
                 <div class="buttons">
-                    <button class="button is-primary" onClick={CreateFlashCard}>Add Card</button>
-                    <button class="button is-success" onClick={SetTimer}>Next</button>
+                    <button class="button is-success">ANSWER</button>
                 </div>
             </div>
 
-
-            {/* Flashcard Number */}
-            <div className={`${create.cardNumber}`}>
-                <p>{`${cardCounter}/${cardCounter}`}</p>
-            </div>
 
 
         </>
--- flashcard-app/pages/create.js
+++ flashcard-app/pages/create.js
@@ -5,7 +5,6 @@ import create from '../styles/Create.module.css'
 import Link from 'next/link'
 import { useState } from "react"
 import { Router, useRouter } from 'next/router'
-import { useEffect } from "react"
 
 // Load Chance
 var Chance = require('chance');
@@ -39,14 +38,22 @@ export default function Create(){
     
     // Set the quiz time 
     let TotalQuizTime = 0
+
+    let IdForClock = ""
+
     
 
 
     // ------- Spaced Repetition ----------- 
     let blocks = [[],[],[],[],[]]
-    blocks[0] = FlashCards
 
-    let [score, setScore] = useState(0)
+    blocks[0] = FlashCards // Add all the flash cards to the first block to start
+
+    //timers for each block 
+    const BlockTimers = {1:30000,2:25000,3:20000,4:15000,5:10000}
+
+    // Keep track of score 
+    let score = 0
 
 
 
@@ -118,6 +125,8 @@ export default function Create(){
         Card.push(inputQuestions)
         Card.push(answer)
         Card.push(date)
+        let cardIdNumber = chance.integer({ min: 0, max: 1000 }) // Generate a random id number for the card 
+        Card.push(cardIdNumber)
 
         let cards = [Card]
         // Append it to collection of FlashCards 
@@ -354,19 +363,35 @@ export default function Create(){
             if(start == 0){
                 clearInterval(startID)
                 console.log('Timer Ended')
+
+
+                // Begin the flashcard quiz 
+                //  Set Up the material for the quiz 
+                StartTimer()
+                PresentCards()
             }
 
         }, 1000);
 
 
-        // Set the quiz time 
-        PickQuestion()
+
 
     }
 
 
 
-    // Choose box 
+    //  Start the quiz 
+    function StartTimer(){
+
+        // After the quiz time ends itll activate the EndQuiz function 
+        setTimeout(EndQuiz,TotalQuizTime) 
+    }
+
+
+
+
+
+    // Choose block using weighted chance 
     function ChooseBox(){
         let randomBox = chance.weighted(['1', '2','3','4','5'], [50,26,14,7,3])
         return randomBox
@@ -381,6 +406,7 @@ export default function Create(){
 
 
     // Pick a question and start time 
+    // returns an array [randomQuestion, blockNumber]
     function PickQuestion(){
 
         // picks a random block based off weighted numbers 
@@ -435,7 +461,326 @@ export default function Create(){
 
 
 
+    // Starts the flashcards
+    function PresentCards(){
 
+        // Generates a question and block number 
+        let options = PickQuestion()
+        let question = options[0]
+        let block = options[1]
+
+        // Get the question time duration 
+        let getQuestionTime = BlockTimers[block]
+        let questionTime = getQuestionTime/1000
+
+        // Set the countdown timer for the set question 
+        setStartCountDown(questionTime)
+
+        let start = questionTime
+
+
+        BuildCard(question,block)
+
+        try{
+            clearInterval(IdForClock)
+
+        }catch{
+            console.log('No Clock Found')
+        }
+
+        let startID = setInterval(function(){ 
+            IdForClock = startID
+
+            start = start -  1
+            setStartCountDown(start)
+
+            // End timer once quiz time ends 
+            if(start == 0){
+                clearInterval(startID)
+                console.log('Timer Ended')
+
+                SubmitAnswer(TimerRanOut = true)
+            }
+
+        }, 1000);    
+
+
+    }
+
+
+    // Builds the flashcard layout 
+    function BuildCard(CardQueston,block){
+
+        let MainQuestion = CardQueston[0] // question 
+        let PossibleAnswers = CardQueston[1] // possible answer 
+
+        // Disable and enable the question text area to add the main question 
+        document.getElementById('mainQuestion').innerHTML = MainQuestion
+
+
+        /// ----- ANSWERS CREATED ---- 
+        for(let i=0; i <= PossibleAnswers.length -1 ; i ++){
+
+            FillCardInfo(PossibleAnswers[i])
+      
+        }
+
+
+        // Submit button 
+        let AnswerButton = document.createElement('button')
+        AnswerButton.className = "button is-success answerbutton"
+        AnswerButton.id = "QuizAnswerButton"
+        AnswerButton.innerHTML = "Answer"
+        AnswerButton.style = "margin: 0 auto"
+        AnswerButton.onclick = function(){
+            SubmitAnswer(CardQueston,block)
+        }
+
+        let FlashCardQuizBox = document.getElementById('NewCard')
+        FlashCardQuizBox.appendChild(AnswerButton)
+
+    }
+
+
+
+
+    //  Generates the flashcard info for the quiz 
+
+    function FillCardInfo(possibleAnswer){
+        
+        //  Create the div box to hold the radio button and answer 
+        let divWithRadioButtons = document.createElement('div')
+        divWithRadioButtons.className = "control withRadioButton"
+        divWithRadioButtons.id = "answer_box_" + possibleAnswer.split(' ').join('-')
+
+        // Create the div for the radio box 
+        let RadioButtonBox = document.createElement('div')
+        RadioButtonBox.className = " control RadioButtonBox "
+
+        // Create radio button
+        let RadioButtonInput = document.createElement('input')
+        RadioButtonInput.type = 'radio'
+        RadioButtonInput.id = "radio_button_" + possibleAnswer.split(' ').join('-')
+        RadioButtonInput.value = possibleAnswer
+        RadioButtonInput.name = "quizRadioButtonAnswer"
+
+        // Append radio button to RadioButtonBox 
+        RadioButtonBox.appendChild(RadioButtonInput)
+
+
+
+
+        // Create the div for the answer box 
+        let AnswerBox = document.createElement('div')
+        AnswerBox.className = "control answers"
+        
+        // Create a text input field for possible answer
+        let input = document.createElement("input");
+        input.type = "text";
+        input.className = "input";
+        input.placeholder = possibleAnswer
+        input.id = "text_input_" + possibleAnswer.split(' ').join('-')
+        input.disabled = true
+        
+        // Add the input field to the answer div container 
+        AnswerBox.appendChild(input)
+
+
+
+
+        // Stack all the elements 
+        divWithRadioButtons.appendChild(RadioButtonBox)
+        divWithRadioButtons.appendChild(AnswerBox)
+
+
+
+
+
+        // Add the text input field to the questions box 
+        document.getElementById('quizAnswerInput').appendChild(divWithRadioButtons)
+    }
+
+
+
+
+    // Takes the users answer input and checks to see if they are correct 
+    function SubmitAnswer(CardQueston,block,TimerRanOut = false){
+
+        // See if the user picked the correct answer
+        let correctAnswer = 'radio_button_' + CardQueston[2].split(" ").join("-")
+        // Gets correct answer 
+        let ChooseCorrectly  = document.getElementById(correctAnswer).checked 
+       
+
+        // If the timer ran out 
+        if(TimerRanOut == true){
+            PickedIncorrectly(CardQueston)
+            ClearQuizCards()
+            PresentCards()
+
+            
+        }
+
+
+
+        // If chose correctly then move question to next block level and add 2 points 
+        if(ChooseCorrectly == true){
+
+            PickedCorrect(CardQueston)
+            ClearQuizCards()
+            PresentCards()
+            
+
+        } else {
+            PickedIncorrectly(CardQueston)
+            ClearQuizCards()
+            PresentCards()
+        }
+
+
+
+
+
+        // Log score
+        console.log('Score',score)
+
+    }
+
+
+
+    // Steps to resolve when selecting correct answer 
+    function PickedCorrect(CardQueston){
+        
+        // Add 2pts to score 
+        score = score + 2
+
+        window.alert('You are correct');
+
+        // Find the flashcard in the block and move it up another level and add two points 
+        console.log('current block is')
+
+        // Loops through each block 
+        let blockCount = 0
+        blocks.map(index=> {
+            
+
+            // If the blocks has flashcards then it loops through the blocks flashcards 
+            if(index.length > 0){
+
+                let indexCount = 0 
+
+                // Loops through each array 
+                index.map(array =>{ 
+                    // If the id's match return the index of the card in the block 
+                    if(array[4] == CardQueston[4]){
+
+                        console.log('block count is',blockCount)
+                        // Add it to the fifth block
+                        if(blockCount => 5 ){
+                            blocks[blockCount].push(blocks[blockCount][indexCount]) // Adds the array to the next level block 
+                        } else {
+                            blocks[blockCount+1].push(blocks[blockCount][indexCount])
+                        }
+
+
+                        blocks[blockCount].splice(indexCount,1) // Deletes the question from the block  
+                        return console.log(score)
+                    }
+                })
+
+                indexCount = indexCount + 1 
+            }
+
+            blockCount = blockCount + 1 
+
+
+        })
+    }
+
+
+    function PickedIncorrectly(CardQuestion){
+        window.alert('You are wrong');
+        score = score - 2
+
+        // Loops through each block 
+        let blockCount = 0
+        blocks.map(index=> {
+            
+
+            // If the blocks has flashcards then it loops through the blocks flashcards 
+            if(index.length > 0){
+
+                let indexCount = 0 
+
+                // Loops through each array 
+                index.map(array =>{ 
+                    // If the id's match return the index of the card in the block 
+                    if(array[4] == CardQuestion[4]){
+
+                        console.log('block count is',blockCount)
+                        // Add it to the fifth block
+                        if(blockCount <= 0  ){
+                            blocks[blockCount].push(blocks[blockCount][indexCount]) // Adds the array to the next level block 
+                        } else {
+                            blocks[blockCount-1].push(blocks[blockCount][indexCount])
+                        }
+
+
+                        blocks[blockCount].splice(indexCount,1) // Deletes the question from the block 
+                        return console.log(score)
+                    }
+                })
+
+                indexCount = indexCount + 1 
+            }
+
+            blockCount = blockCount + 1 
+
+
+        })
+    }
+
+
+    // Resets the cards for answers 
+    function ClearQuizCards(){
+        
+        // Deletes all possible answers and radio buttons
+        function removeAllChildNodes(parent) {
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+        }
+        const container = document.querySelector('#quizAnswerInput')
+        removeAllChildNodes(container);
+
+
+        // Delete Answer Button
+        const button = document.getElementById('QuizAnswerButton')
+        button.remove()
+
+
+    }
+
+
+    function EndQuiz(){
+
+        window.alert('Your Quiz Has Ended')
+        
+        // remove the quiz box 
+        document.getElementById('quizbox').remove()
+        document.getElementById('scorebox').style.display = "flex"
+
+        // Add the score text 
+        let scoreTextDiv = document.getElementById('scorenumber')
+        let scoreText = document.createElement('p')
+        scoreText.className = "title has-text-black"
+        scoreText.innerHTML = "Your score is " + score.toString()
+
+        scoreTextDiv.appendChild(scoreText)
+
+    
+
+    }
 
 
 
@@ -562,15 +907,45 @@ export default function Create(){
                 {/* START THE FLASHCARDS QUIZ */}
                 <div id="quizbox" className={`hero-body box center ${create.createBox}`}>
                     {/* Counter */}
+
                     <div className="box" id="count_down_box">
                         <p className="title has-text-black">{startCountDown}</p>
                     </div>
 
-                    <p>Ready</p>
+                    <div id="NewCard">
+
+                        {/* ENTER QUESTION SECTION */}
+                        <div className={`${create.questionContainer}`}>
+                            <div class="field">
+                                <div class="control">
+                                    <p id="mainQuestion">Your Quiz Will Begin Shortly</p>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div id="quizAnswerInput">
+
+
+                        </div>
+
+
+
+
+                        
+                    </div>
 
                 </div>
 
 
+
+
+                <div id="scorebox" className={`hero-body center ${create.createBox}`}>
+                    <div id="scorenumber" className="box">
+                    </div>
+
+                </div>
 
                 <div className="hero-foot center">
                     <p>Github</p>
